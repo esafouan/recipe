5e1a8eeaac52fd146ff3951cdc9cafe6b2ce0c9c
el-mahdi safouane <elmahdi.safouane@veltiston.ai>
2026-02-11 21:24:16 +0100

    grow

--- apps/recipe-blog/app/layout.tsx
+++ apps/recipe-blog/app/layout.tsx
@@ -78,13 +78,28 @@ export default function RootLayout({
         {/* ========================= */}
         {/* 🌱 GROW.ME - Social Growth Tool */}
         {/* ========================= */}
-        <Script
-          id="grow-me-init"
-          strategy="afterInteractive"
-          dangerouslySetInnerHTML={{
-            __html: `!(function(){window.growMe||((window.growMe=function(e){window.growMe._.push(e);}),(window.growMe._=[]));var e=document.createElement("script");(e.type="text/javascript"),(e.src="https://faves.grow.me/main.js"),(e.defer=!0),e.setAttribute("data-grow-faves-site-id","U2l0ZTo0NTdiZDRhNS0zMWJmLTQ4MGUtYTFkOC03NzM2MjVlZDNlMDk=");var t=document.getElementsByTagName("script")[0];t.parentNode.insertBefore(e,t);})();`,
-          }}
-        />
+          <Script id="grow-me" strategy="afterInteractive">
+          {`
+            !(function(){
+              window.growMe || (
+                (window.growMe = function(e){
+                  window.growMe._.push(e);
+                }),
+                (window.growMe._ = [])
+              );
+              var e = document.createElement("script");
+              e.type = "text/javascript";
+              e.src = "https://faves.grow.me/main.js";
+              e.defer = true;
+              e.setAttribute(
+                "data-grow-faves-site-id",
+                "U2l0ZTo0NTdiZDRhNS0zMWJmLTQ4MGUtYTFkOC03NzM2MjVlZDNlMDk="
+              );
+              var t = document.getElementsByTagName("script")[0];
+              t.parentNode.insertBefore(e, t);
+            })();
+          `}
+        </Script>
 
         {/* ========================= */}
         {/* ⚡ Performance Optimizations */}
